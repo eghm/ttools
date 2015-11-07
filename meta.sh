daa6af9b838723ecce0955b4ce99f6e500877050
Erik G. H. Meade <github-m@eghm.net>
2015-11-07 00:56:49 -1000

    Read id out of SERIESID

--- meta.sh
+++ meta.sh
@@ -1,9 +1,18 @@
-SERIESID="SH999903"
+SERIESID="SH999932" # misc
+
+if [ -f SERIESID ]
+then
+    SERIESID=$(cat SERIESID) 
+fi
+
+
 #OAD="2008-05-10T06:00:00Z"
 export TZ=Zulu
 #TIME="2008-05-11T01:37:00Z"
 STITLE=${PWD##*/}
-NUMFILES=$(ls *.m4v | wc -l)
+#EXTENSION=m4v
+#EXTENSION=mp4
+NUMFILES=$(ls *.$EXTENSION | wc -l)
 if [ "$NUMFILES" -gt "1" ]
 then
     ISEPISODE=true
@@ -11,18 +20,27 @@ else
     ISEPISODE=false
 fi
 ENUM=1
-for f in *.m4v
+for f in *.avi *.mp4 *.mkv *.m4v *.mpg
 do
+    echo "processing $f"
+
     OAD=$(date "+%Y-%m-%dT%H:%M:%SZ")
     TTITLE="$f"
     ETITLE="$f"
+
+    DESC=$TTITLE
+    if [ -f "$f.desc" ]
+    then
+        DESC=$(cat "$f.desc")
+    fi
+
     # title and seriesTitle are the same in examples
     echo "s|TTITLE|$STITLE|g" > "meta.$f.sed"
     echo "s|STITLE|$STITLE|g" >> "meta.$f.sed"
     echo "s|ETITLE|$ETITLE|g" >> "meta.$f.sed"
     echo "s|ENUMBER|$ENUM|g" >> "meta.$f.sed"
     echo "s|ISEPISODE|$ISEPISODE|g" >> "meta.$f.sed"
-    echo "s|DESC|$TTITLE|g" >> "meta.$f.sed"
+    echo "s|DESC|$DESC|g" >> "meta.$f.sed"
     echo "s|SERIESID|$SERIESID|g" >> "meta.$f.sed"
     echo "s|OAD|$OAD|g" >> "meta.$f.sed"
     TIME=$(date "+%Y-%m-%dT%H:%M:%SZ")
@@ -31,5 +49,5 @@ do
     ENUM=`expr $ENUM + 1`
 done;
 rm *.sed
-echo "$SERIESID:$STITLE" >> ../seriesIds.txt
+echo "$SERIESID:$STITLE" >> /ttools/seriesIds.txt
 
